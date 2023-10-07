175f5cf08b06c1222882350babc92d25210c45a1
SuperKaskus <twarikadams@gmail.com>
2023-10-07 15:11:35 +1100

    Added components I created for tours App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,45 @@
+import { useEffect, useState } from "react";
+import Loading from "./components/Loading";
+import Tours from "./components/Tours";
+
 const url = 'https://course-api.com/react-tours-project';
 
+
 const App = () => {
-  return <h2>Tours Starter</h2>;
+
+  const [isLoading, setIsLoading] = useState(true)
+  const [tours, setTours] = useState([])
+
+ // fetching data from url using useEffect
+  const fetchTours = async () => {
+    setIsLoading(true)
+    try {
+      const res = await fetch(url);
+      const tours = await res.json();
+      setTours(tours)
+    } catch (error) {
+  console.error();
+    }
+    setIsLoading(false)
+  }
+  
+  useEffect(()=>{
+      fetchTours()
+  },[])
+
+  //display loading while the data is beign fetched
+  if (isLoading){
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
+  // render the component when tours data is fetched.
+  return (
+  <main>
+    <Tours tours = {tours} />
+   </main>
+  )
 };
 export default App;
---
+++ src/components/Tours.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Tour from './Tour'
+
+function Tours({tours}) {
+
+
+  return (
+    <section>
+        <div className="title">
+            <h2>Our Tours</h2>
+            <div className="title-underline"></div>
+        </div>
+        <div className="tours">
+            {tours.map((tour)=>{
+                console.log(tour)
+                return <Tour key={tour.id} {...tour} />
+            })}
+        </div>
+    </section>
+  )
+}
+
+export default Tours
